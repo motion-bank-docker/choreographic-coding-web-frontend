fc1a16664e4ed8cf0651336d3aa21f7873d5117d
Florian Jenett <fjenett@gmail.com>
2019-03-29 15:45:06 +0100

    untracked changes

--- src/App.vue
+++ src/App.vue
@@ -13,14 +13,14 @@ export default {
 </script>
 
 <style lang="stylus">
-  @import url('https://fonts.googleapis.com/css?family=Alegreya+Sans|Contrail+One')
+  @import url('https://fonts.googleapis.com/css?family=Asar|Quicksand')
 
   :root, html
     font-size 18px
-    font-family 'Alegreya Sans', sans-serif
+    font-family 'Asar', serif
 
   h1, h2, h3, h4, h5, h6
-    font-family 'Contrail One', cursive
+    font-family 'Quicksand', sans-serif
 
   a, a:hover, a:active, a:visited
     color inherit
