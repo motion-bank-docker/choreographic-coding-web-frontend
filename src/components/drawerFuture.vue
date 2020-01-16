54ebe48823f1f64a1f7dc3abfb04a60ff170ea12
Milan G <dev@milangress.de>
2020-01-16 16:08:57 +0100

    Mailchimp subscribe as Vue component

--- src/components/drawerFuture.vue
+++ src/components/drawerFuture.vue
@@ -4,17 +4,27 @@
       p Future Projects / Plans / Goals
       p shape the future with us (da email)
       textarea
-      template(v-for="node in nodes")
-        router-link(:to="{name: 'page.lab', params: { nid: node.nid }}")
-          h3 {{node.title}}
-        div.imgPlaceholder
-        //p(v-html="node.body.value")
-        //div(v-for="img in node.field_images_2")
-          img(v-if="img.file.id in imgs" :src="imgs[img.file.id].path")
+      mailchimp-subscribe(url='https://choreographiccoding.us8.list-manage.com/subscribe/post-json'
+        user-id='5b3dec718900ad84210c545ac'
+        list-id='24abe7a566'
+        @error='onError'
+        @success='onSuccess')
+        template(v-slot='{ subscribe, setEmail, firstName, error, success, loading }')
+          form(@submit.prevent='subscribe')
+            input(type='email' @input='setEmail($event.target.value)')
+            input(type='text' v-bind:value="firstName")
+            button(type='submit') Submit
+            div(v-if='error') {{ error }}
+            div(v-if='success') Yay!
+            div(v-if='loading') Loading&mldr;
 </template>
 
 <script>
+import mailchimpSubscribe from './mailchimpSubscribe'
 export default {
+  components: {
+    mailchimpSubscribe
+  },
   name: 'drawerFuture',
   data () {
     return {
@@ -38,6 +48,12 @@ export default {
     this.imgs = imgsMap
   },
   methods: {
+    onError (e) {
+      console.log(e)
+    },
+    onSuccess (e) {
+      console.log(e)
+    },
     loadImage: async function (id) {
       const resImg = await this.$store.dispatch('drupal/getImgPath', id)
       let file = resImg.data.files[0].file
